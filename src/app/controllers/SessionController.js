931ff1ee8c07e30e8a4889257b283a4c2d8127de
Lucas Pedro Lopes <lucaspedrolbg@gmail.com>
2019-08-06 01:38:08 -0300

    Autenticação de Usuário. sessecton

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not fout!' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+  }
+}
