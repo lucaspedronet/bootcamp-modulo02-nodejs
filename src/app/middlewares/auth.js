106f0406b83d96806e21efb140cd466df9ea42cf
Lucas Pedro Lopes <lucaspedrolbg@gmail.com>
2019-08-10 01:57:14 -0300

    Documentando Middleware

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,3 +1,6 @@
+/**
+ * @function promisify Nos permite transformar um modelo function de `callbac/promisi` para `Async/Await`.
+ */
 import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 import authConfug from '../../config/auth';
@@ -12,11 +15,19 @@ export default async (req, res, next) => {
   const [, token] = authHeader.split(' ');
 
   try {
+    /**
+     * @constant decoded `promisify`(jwt.verify)(token, authConfug.secret) a function promisify transforma a function verify() da lib jwt.
+     * em seguida retorna em um segundo parâmetro outras duas váriaveis `(token, authConfug.secret)`, neste momento o *jwt.verify*
+     * esta realizando uma verificação/comparação entre *token* e *authConfug.secret*. Caso não seja iguis os token, irá cair diretamente
+     * para o cath.
+     */
     const decoded = await promisify(jwt.verify)(token, authConfug.secret);
 
-    console.log(decoded);
-
+    /**
+     * @variation req.userId uma nova variável criada em tempo de execução, que vai armazenar o id do user
+     */
     req.userId = decoded.id;
+
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
